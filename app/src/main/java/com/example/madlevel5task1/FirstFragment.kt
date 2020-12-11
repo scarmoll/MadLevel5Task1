deb273c862eb4cfe46c0045361e3d2afb266f966
Oscar Mollé <scarmoll@gmail.com>
2020-12-11 17:39:20 +0100

    first fragment updated

--- app/src/main/java/com/example/madlevel5task1/FirstFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/FirstFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private val viewModel: NoteViewModel by viewModels()
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -24,8 +27,16 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        observeAddNoteResult()
+    }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            note?.let {
+                tvNoteTitle.text = it.title
+                tvLastUpdated.text = getString(R.string.last_updated, it.lastUpdated.toString())
+                tvNoteText.text = it.text
+            }
+        })
     }
 }
